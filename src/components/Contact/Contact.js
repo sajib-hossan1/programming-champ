6296fbe3bf4c3d3d449f81172bbf2b72b71d852c
sajib-hossan1 <sajib.hossan112@gmail.com>
2022-07-07 21:33:26 +0600

    final updated

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -12,35 +12,35 @@ const Contact = () => {
                             <h3>Get In Touch</h3>
                             <span>If you have any query about us, feel free to message us.</span>
                             <div className='mb-2'>
-                                <i class="fa-solid fa-phone"></i>
+                                <i className="fa-solid fa-phone"></i>
                                 <span>Phone : +21 123456789</span>
                             </div>
                             <div className='mb-2'>
-                                <i class="fa-solid fa-location-dot"></i>
+                                <i className="fa-solid fa-location-dot"></i>
                                 <span>Location : San Francisco, USA</span>
                             </div>
                             <div className='mb-2'>
-                                <i class="fa-solid fa-clock"></i>
-                                <span>Business Hours : 9:00 - 5:00 (Sun to Thu)</span>
+                                <i className="fa-solid fa-clock"></i>
+                                <span>Business Hours : 9am - 5pm (Sun to Thu)</span>
                             </div>
                         </div>
                     </div>
                     <div className="col-md-12 col-lg-6">
                         <div className="contact-from-container">
                             <form className='container-from shadow'>
-                                <div class="form-group mb-3">
-                                    <label className='mb-2' for="exampleFormControlTextarea3">Your Message</label>
-                                    <textarea class="form-control" id="exampleFormControlTextarea3" rows="5"></textarea>
+                                <div className="form-group mb-3">
+                                    <label className='mb-2' htmlFor="exampleFormControlTextarea3">Your Message</label>
+                                    <textarea className="form-control" id="exampleFormControlTextarea3" rows="5"></textarea>
                                 </div>
-                                <div class="form-group mb-3">
-                                    <label className='mb-2' for="exampleInputText">Your Name</label>
-                                    <input type="text" class="form-control" id="exampleInputText" aria-describedby="emailHelp"/>
+                                <div className="form-group mb-3">
+                                    <label className='mb-2' htmlFor="exampleInputText">Your Name</label>
+                                    <input type="text" className="form-control" id="exampleInputText" aria-describedby="emailHelp"/>
                                 </div>
-                                <div class="form-group mb-3">
-                                    <label className='mb-2' for="exampleInputEmail1">Email Address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                <div className="form-group mb-3">
+                                    <label className='mb-2' htmlFor="exampleInputEmail1">Email Address</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                                 </div>
-                                <button type="submit" class="custom-btn2 contact-btn">Send Message</button>
+                                <button type="submit" className="custom-btn2 contact-btn">Send Message</button>
                             </form>
                         </div>
                     </div>
--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -9,75 +9,75 @@ const Faq = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-lg-6 col-md-12">
-                        <div class="accordion accordion-flush accordation-container" id="accordionFlushExample">
-                            <div class="accordion-item mb-3">
-                                <h2 class="accordion-header" id="flush-headingOne">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        <div className="accordion accordion-flush accordation-container" id="accordionFlushExample">
+                            <div className="accordion-item mb-3">
+                                <h2 className="accordion-header" id="flush-headingOne">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
                                         How We Teach?
                                     </button>
                                 </h2>
-                                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
+                                <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                    <div className="accordion-body">
                                         This is a online based learning platfrom. We release video with module type and student can see their particular course video from our website.
                                     </div>
                                 </div>
                             </div>
-                            <div class="accordion-item mb-3">
-                                <h2 class="accordion-header" id="flush-headingTwo">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                            <div className="accordion-item mb-3">
+                                <h2 className="accordion-header" id="flush-headingTwo">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
                                     What things do you need for enrollment?
                                 </button>
                                 </h2>
-                                <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
+                                <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                                    <div className="accordion-body">
                                         You must have to your personal computer or laptop to do coding practice with us and a good internet connection to see our buffer less video. You have to know little about computer knowledge.
                                     </div>
                                 </div>
                             </div>
-                            <div class="accordion-item mb-3">
-                                <h2 class="accordion-header" id="flush-headingThree">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                            <div className="accordion-item mb-3">
+                                <h2 className="accordion-header" id="flush-headingThree">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
                                     Our Support System
                                 </button>
                                 </h2>
-                                <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
+                                <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                                    <div className="accordion-body">
                                         By practicing code you will definitely got stcuk. Then simply you can email or post your particular course group or On the other hand we have live support session in our web site you can simply click support {'>'} click your course {'>'} get the solutions of your problem.
                                     </div>
                                 </div>
                             </div>
-                            <div class="accordion-item mb-3">
-                                <h2 class="accordion-header" id="flush-headingFour">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+                            <div className="accordion-item mb-3">
+                                <h2 className="accordion-header" id="flush-headingFour">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
                                     Our Payment Method
                                 </button>
                                 </h2>
-                                <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
+                                <div id="flush-collapseFour" className="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+                                    <div className="accordion-body">
                                         We accept ssl commerz. You can simply payment us with ssl commerz.
                                     </div>
                                 </div>
                             </div>
-                            <div class="accordion-item mb-3">
-                                <h2 class="accordion-header" id="flush-headingFive">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+                            <div className="accordion-item mb-3">
+                                <h2 className="accordion-header" id="flush-headingFive">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
                                     Refund Policy
                                 </button>
                                 </h2>
-                                <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
+                                <div id="flush-collapseFive" className="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+                                    <div className="accordion-body">
                                         Our refund policy is very simple you can read this from <a href="policy">here</a>
                                     </div>
                                 </div>
                             </div>
-                            <div class="accordion-item mb-3">
-                                <h2 class="accordion-header" id="flush-headingSix">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
+                            <div className="accordion-item mb-3">
+                                <h2 className="accordion-header" id="flush-headingSix">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
                                     Do you have more questions?
                                 </button>
                                 </h2>
-                                <div id="flush-collapseSix" class="accordion-collapse collapse" aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
+                                <div id="flush-collapseSix" className="accordion-collapse collapse" aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
+                                    <div className="accordion-body">
                                         If you have more questions you simply message us using our website or you can send message here : programmingchamp@gmail.com
                                     </div>
                                 </div>
